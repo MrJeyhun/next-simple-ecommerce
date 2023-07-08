30307e0e01b05dbc14b4f0ffab31f0e0494258db
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-07-08 12:20:59 +0300

    initialized stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { useState } from 'react';
 import Head from 'next/head';
 import styles from './page.module.css';
 
 import products from 'public/mock/products.json';
+import { initiateCheckout } from 'lib/payments';
 
 export default function Home() {
     return (
@@ -17,83 +19,36 @@ export default function Home() {
 
                 <p className={styles.description}>Real style must be simple!</p>
 
-                {/**
-                 * @lesson-02-todo Exercise 1
-                 * We want to add products to our app. How can we use the code that
-                 * already comes with Next.js to get started adding our products?
-                 *
-                 * @lesson-02-todo Exercise 2
-                 * Our product details are important, but it's also important to show
-                 * some images of our products. Let's import pictures of our products
-                 * and display those images above our product titles.
-                 *
-                 * @lesson-02-todo Exercise 3
-                 * Using an unordered list typically makes more sense semantically when
-                 * showing a list of something like in our case, products. Update the
-                 * grid of products to an unordered list with a list item that wraps
-                 * each product card.
-                 */}
-
                 <ul className={styles.grid}>
                     {products.map(product => {
                         return (
                             <li className={styles.card} key={product.id}>
                                 <a href="#">
-                                    <img src={product.image} alt={product.title} />
+                                    <img src={product.image} alt={product.title} className={styles.img_md} />
                                     <h3>{product.title}</h3>
                                     <p>${product.price}</p>
                                     <p>{product.description}</p>
                                 </a>
+                                <p>
+                                    <button
+                                        className={styles.buyButton}
+                                        onClick={() =>
+                                            initiateCheckout({
+                                                lineItems: [
+                                                    {
+                                                        price: product.id,
+                                                        quantity: 1,
+                                                    },
+                                                ],
+                                            })
+                                        }
+                                    >
+                                        Buy Now!
+                                    </button>
+                                </p>
                             </li>
                         );
                     })}
-                    {/* <li className={styles.card}>
-                        <a href="https://nextjs.org/docs">
-                            <img src="/images/colorfuljacket.jpg" alt="colorfuljacket" />
-                            <h3>Title</h3>
-                            <p>Description</p>
-                        </a>
-                    </li>
-
-                    <li className={styles.card}>
-                        <a href="https://nextjs.org/learn">
-                            <img src="/images/jacket.jpg" alt="jacket" />
-                            <h3>Title</h3>
-                            <p>Description</p>
-                        </a>
-                    </li>
-
-                    <li className={styles.card}>
-                        <a href="https://github.com/vercel/next.js/tree/master/examples">
-                            <img src="/images/milada.jpg" alt="milada" />
-                            <h3>Nice crop</h3>
-                            <p>Nice as you</p>
-                        </a>
-                    </li>
-
-                    <li className={styles.card}>
-                        <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-                            <img src="/images/pinktshirt.jpg" alt="pinktshirt" />
-                            <h3>Title</h3>
-                            <p>Description</p>
-                        </a>
-                    </li>
-
-                    <li className={styles.card}>
-                        <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-                            <img src="/images/blacktshirt.jpg" alt="blacktshirt" />
-                            <h3>Title</h3>
-                            <p>Description</p>
-                        </a>
-                    </li>
-
-                    <li className={styles.card}>
-                        <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-                            <img src="/images/whitetshirt.jpg" alt="whitetshirt" />
-                            <h3>Title</h3>
-                            <p>Description</p>
-                        </a>
-                    </li> */}
                 </ul>
             </main>
 
